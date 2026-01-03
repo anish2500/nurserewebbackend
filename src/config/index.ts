95a282571e306fdfef476d711c5f6147cbf7bce4
anish2500 <tmganish2500@gmail.com>
2026-01-03 17:20:04 +0545

    final changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,9 @@ dotenv.config();
 export const PORT: number = 
     process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const MONGODB_URI: string = 
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/35a_backend';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/35a_backend';// this is the backup database 
 // Application level constants, with fallbacks 
-// if .env variables are not set
+
 
 export const JWT_SECRET: string = 
     process.env.JWT_SECRET || 'default_secret'
